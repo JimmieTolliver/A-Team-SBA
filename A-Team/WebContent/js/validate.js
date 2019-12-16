1b5e3565b2bc780bc34c4274ea1cf86aef3294d0
josep <josep@COLD-SURFACE>
2019-12-16 17:50:21 -0500

    Login page update with new form and background image.

--- A-Team/WebContent/js/validate.js
+++ A-Team/WebContent/js/validate.js
@@ -1,6 +1,6 @@
 function validate() {
     'use strict';
-    var password = document.getElementById('pass').value, validPass = new RegExp("^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#\$%\^&\*])(?=.{8,})"), passResult = validPass.test(password);
+    var password = document.getElementById('password').value, validPass = new RegExp("^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#\$%\^&\*])(?=.{8,})"), passResult = validPass.test(password);
         
     if (passResult === false) {
         alert('Enter a valid password');
